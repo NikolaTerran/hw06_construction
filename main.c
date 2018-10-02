5b0ddc7d976e6ac1399fbd6d80108dab165b12d2
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-02 11:54:46 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+##include <stdio.h>
+#include <string.h>
+
+struct profile {char ini; char* name;};
+struct profile studentA;
+
+void struct_print(struct profile student){
+        printf("student first initial: %c\n",student.ini);
+        printf("student last name: %s\n",student.name);
+}
+
+struct profile* struct_set(char a, char* b, struct profile student){
+        (*student).ini
+char * struct_get(){
+
+}
+
+int main(){
+
+//demo struct_get
+
+//demo struct_set
+
+//
+
+return 0;
+}
