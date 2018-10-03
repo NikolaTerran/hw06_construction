d1864a751bcbc3603ce448e24e4e5372cf98ad56
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-03 11:48:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct profile {char ini; char* name;};
 struct profile studentA;
+struct profile studentB;
+
+const char *rand_name(){
+    RAND_MAX = 10;
+    const char *name_set[10];
+    name_set[0] = "jimmy";
+    name_set[1] = "cartman";
+    name_set[2] = "kenny";
+    name_set[3] = "timmy";
+    name_set[4] = "tokken";
+    name_set[5] = "clyde";
+    name_set[6] = "james";
+    name_set[7] = "ike";
+    name_set[8] = "kyle";
+    name_set[9] = "stan";
+    return name_set[rand()];
+}
 
 void struct_print(struct profile student){
         printf("student first initial: %c\n",student.ini);
@@ -23,5 +41,9 @@ struct_set('t',"liu",&studentA);
 //demo struct_print
 struct_print(studentA);
 
+struct_set('t',rand_name(),&studentB);
+struct_print(studentB);
+
 return 0;
 }
+
